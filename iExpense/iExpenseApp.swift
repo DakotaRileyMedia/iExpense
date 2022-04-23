deb7796fbe7d3c5d595f42bac0a08914f5e93f0d
DakotaRileyMedia <dakotacustompcs@gmail.com>
2022-04-23 13:13:29 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dakota Riley on 4/23/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
